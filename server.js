d2495f1b079471eaece386e507e5724c00e718ad
AndreasIrta <andreasirta0711@gmail.com>
2021-07-30 01:45:48 -0600

    add moongose connect

--- server.js
+++ server.js
@@ -15,8 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget-tracker", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgetTracker", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
